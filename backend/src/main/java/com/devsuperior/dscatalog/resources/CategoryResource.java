5ef08abb7cf2f3e7ebd3b07fb5b206a46408b142
André Luis <andrelluis17@hotmail.com>
2022-01-25 12:41:25 -0300

    PUT category

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -8,14 +8,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
-import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.services.CategoryService;
 
 @RestController
@@ -38,9 +37,15 @@ public class CategoryResource {
 	}
 	
 	@PostMapping
-	public ResponseEntity<CategoryDTO> save(@RequestBody CategoryDTO dto) {
-		dto = service.save(dto);
+	public ResponseEntity<CategoryDTO> saveCategory(@RequestBody CategoryDTO dto) {
+		dto = service.saveCategory(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
 		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> updateCategory(@PathVariable Long id , @RequestBody CategoryDTO dto) {
+		dto = service.updateCategory(id , dto);
+		return ResponseEntity.ok().body(dto);
 	}
 }
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
-import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -28,13 +30,21 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		 Optional<Category> obj = repository.findById(id);
-		 Category category = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		 Category category = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		 return new CategoryDTO(category);
 	}
 	
 	@Transactional
-	public CategoryDTO save(CategoryDTO dto) {
+	public CategoryDTO saveCategory(CategoryDTO dto) {
 		Category category = new Category(null , dto.getName());
+		category = repository.save(category);
+		return new CategoryDTO(category);
+	}
+	
+	@Transactional
+	public CategoryDTO updateCategory(Long id , CategoryDTO dto) {
+		findById(id);
+		Category category = new Category(id , dto.getName());
 		category = repository.save(category);
 		return new CategoryDTO(category);
 	}
