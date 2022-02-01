9d7f6e954dcb2092c92bc7854ecf47e052ff7a04
André Luis <andrelluis17@hotmail.com>
2022-02-01 16:39:39 -0300

    Utilitario para fazer requisiçoes http, Listando produtos dinamicamente, Exibindo informaçoes dos produtos incompleto ainda

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,23 +1,36 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import ProductCard from './components/ProductCard';
 import './styles.scss';
+import { makeRequest } from '../../core/utils/request';
+import { ProductsResponse } from '../../core/types/Product';
 
-const Catalog = () => (
-    <div className="catalog-container">
-        <h1 className='catalog-title'>
-            Catálogo de produtos
-        </h1>
-        <div className='catalog-products'>
-            <Link to="/products/1"><ProductCard /></Link>
-            <Link to="/products/2"><ProductCard /></Link>
-            <Link to="/products/3"><ProductCard /></Link>
-            <Link to="/products/4"><ProductCard /></Link>
-            <Link to="/products/5"><ProductCard /></Link>
-            <Link to="/products/6"><ProductCard /></Link>
-            <Link to="/products/7"><ProductCard /></Link>
-            <Link to="/products/8"><ProductCard /></Link>
+const Catalog = () => {
+    const [productResponse, setProductResponse] = useState<ProductsResponse>();
+
+    useEffect(() => {
+        const params = {
+            page: 0,
+            linesPerPage: 12
+        }
+        makeRequest({ url: '/products', params: params })
+            .then(response => setProductResponse(response.data))
+    }, [])
+
+    return (
+        <div className="catalog-container">
+            <h1 className='catalog-title'>
+                Catálogo de produtos
+            </h1>
+            <div className='catalog-products'>
+                {productResponse?.content.map(product => (
+                    <Link to="/products/1" key={product.id}>
+                        <ProductCard product={product}/>
+                    </Link>
+                ))}
+            </div>
         </div>
-    </div>
-);
+    )
+};
 
 export default Catalog;
