700bf24f37e33fb630cbb5ad1a572f1bf20448ed
André Luis <andrelluis17@hotmail.com>
2022-02-16 14:38:29 -0300

    form login usando a lib React hook form para gerenciar formularios

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -2,21 +2,38 @@ import ButtonIcon from 'core/components/ButtonIcon';
 import { Link } from 'react-router-dom';
 import AuthCard from '../Card';
 import './styles.scss';
+import { useForm } from 'react-hook-form';
+
+type FormData = {
+    email: string;
+    password: string;
+}
 
 const Login = () => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+
+    const onSubmit = (data: FormData) => {
+        console.log(data);
+        // Fazer a chamada a API
+    }
+
     return (
         <div>
             <AuthCard title="Login">
-                <form className='login-form'>
+                <form className='login-form' onSubmit={handleSubmit(onSubmit)}>
                     <input
                         type="email"
                         className='form-control input-base margin-bottom-30'
                         placeholder='Email'
+                        {...register('email', { required: true })}
                     />
+                    {errors.email && <p>email is required.</p>}
                     <input
                         type="password"
                         className='form-control input-base'
                         placeholder='Senha'
+                        {...register('password')}
                     />
                     <Link to="/admin/auth/recover" className='login-link-recover'>
                         Esqueci a senha?
