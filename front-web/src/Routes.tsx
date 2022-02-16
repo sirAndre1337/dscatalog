f97d5ce919bdc3794ad2460eade3aa12f5415aa2
André Luis <andrelluis17@hotmail.com>
2022-02-16 12:54:58 -0300

    Criando modulo de autenticação , estrutura de rotas do formulario

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -1,3 +1,4 @@
+import Auth from 'pages/Auth';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import Navbar from './core/components/Navbar';
 import Admin from './pages/Admin';
@@ -16,6 +17,10 @@ const Routes = () => (
             </Route>
             <Route path="/products/:productId">
                 <ProductDetails />
+            </Route>
+            <Redirect from='/admin/auth' to="/admin/auth/login" exact/>
+            <Route path="/admin/auth">
+                <Auth />
             </Route>
             <Redirect from='/admin' to="/admin/products" exact/>
             <Route path="/admin">
