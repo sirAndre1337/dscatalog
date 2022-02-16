7ab21286e45d283703e4fae8bb7e0b9737c0121d
André Luis <andrelluis17@hotmail.com>
2022-02-16 13:43:03 -0300

    Criando formulario de login

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,3 +1,5 @@
+import ButtonIcon from 'core/components/ButtonIcon';
+import { Link } from 'react-router-dom';
 import AuthCard from '../Card';
 import './styles.scss';
 
@@ -6,12 +8,35 @@ const Login = () => {
         <div>
             <AuthCard title="Login">
                 <form className='login-form'>
-                    <h1>Form Login</h1>
+                    <input
+                        type="email"
+                        className='form-control input-base margin-bottom-30'
+                        placeholder='Email'
+                    />
+                    <input
+                        type="password"
+                        className='form-control input-base'
+                        placeholder='Senha'
+                    />
+                    <Link to="/admin/auth/recover" className='login-link-recover'>
+                        Esqueci a senha?
+                    </Link>
+                    <div className='login-submit'>
+                        <ButtonIcon text="Logar" />
+                    </div>
+                    <div className='text-center'>
+                        <span className='not-registered'>
+                            Não tem cadastro?
+                        </span>
+                        <Link to="/admin/auth/register" className='login-link-register'>
+                            CADASTRAR
+                        </Link>
+                    </div>
                 </form>
             </AuthCard>
         </div>
     )
 }
-    
+
 
 export default Login;
