1203b152f803e0361090c0f28f0eaae0ccc4469f
André Luis <andrelluis17@hotmail.com>
2022-04-03 22:58:26 -0300

    NavBar for smarthphone

--- front-web/src/core/components/Navbar/index.tsx
+++ front-web/src/core/components/Navbar/index.tsx
@@ -2,12 +2,14 @@ import './styles.scss';
 import { Link, NavLink, useLocation } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 import { getAccessTokenDecode, logout } from 'core/utils/auth';
+import menu from 'core/assets/images/menu.svg';
 
 
 const Navbar = () => {
 
-    const [user,setUser] = useState('');
+    const [user, setUser] = useState('');
     const location = useLocation();
+    const [drawerActive, setDrawerActive] = useState(false);
 
     useEffect(() => {
         const userData = getAccessTokenDecode();
@@ -21,48 +23,81 @@ const Navbar = () => {
 
     return (
         <nav className="bg-primary main-nav">
-            
-                <Link to="/" className='nav-logo-text'>
-                    <h4>Ds Catalog</h4>
-                </Link>
-           
-            <div className="menu-container">
+
+            <Link to="/" className='nav-logo-text' onClick={() => setDrawerActive(false)}>
+                <h4>Ds Catalog</h4>
+            </Link>
+
+            <button className="menu-mobile-btn" type='button' onClick={() => setDrawerActive(!drawerActive)}>
+                <img src={menu} alt="Mobile Menu" />
+            </button>
+
+            <div className={drawerActive ? "menu-mobile-container" : "menu-container"}>
                 <ul className='main-menu'>
                     <li>
-                        <NavLink className='nav-link' to="/" exact>
+                        <NavLink className='nav-link' to="/" exact onClick={() => setDrawerActive(false)}>
                             HOME
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink className='nav-link' to="/products">
+                        <NavLink className='nav-link' to="/products" onClick={() => setDrawerActive(false)}>
                             CATÁLOGO
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink className='nav-link' to="/admin">
+                        <NavLink className='nav-link' to="/admin" onClick={() => setDrawerActive(false)}>
                             ADMIN
                         </NavLink>
                     </li>
+                    {
+                        drawerActive && (
+                            <li>
+                                {user && (
+                                    <a href="#logout" className='nav-link active d-inline'
+                                        onClick={(e) => {
+                                            setDrawerActive(false);
+                                            handleLogout(e);
+                                        }}>
+                                        {`LOGOUT - ${user}`}
+                                    </a>
+                                )}
+                            </li>
+                        )}
+                    {
+                        drawerActive && (
+                            <>
+                                {
+                                    !user && (
+                                        <li>
+                                            <Link to="/auth/login" className='nav-link active' onClick={() => setDrawerActive(false)}>
+                                                LOGIN
+                                            </Link>
+                                        </li>
+                                    )
+                                }
+                            </>
+                        )
+                    }
                 </ul>
             </div>
-            <div className='nav-login'>
-            {user && (
-                <>
+            <div className='user-info-dnone nav-login'>
+                {user && (
+                    <>
                         {user}
-                    <a
-                        href="#logout"
-                        className='nav-link active d-inline'
-                        onClick={handleLogout}
+                        <a
+                            href="#logout"
+                            className='nav-link active d-inline'
+                            onClick={handleLogout}
                         >
-                        LOGOUT
-                    </a>
-                </>
-            )}
-            {!user && (
+                            LOGOUT
+                        </a>
+                    </>
+                )}
+                {!user && (
                     <Link to="/auth/login" className='nav-link active'>
                         LOGIN
                     </Link>
-            )}
+                )}
             </div>
         </nav>
     )
